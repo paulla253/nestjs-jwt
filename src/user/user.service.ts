af468b170d2f2fb7650dc1df2657dfa568b9481e
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-08-14 20:51:11 -0300

    feat: create service user

---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { TUser } from './user.type';
+
+@Injectable()
+export class UserService {
+  private readonly users = [
+    {
+      id: 1,
+      login: 'john',
+      password: '123',
+    },
+    {
+      id: 2,
+      login: 'maria',
+      password: '123',
+    },
+    {
+      id: 3,
+      login: 'ana',
+      password: '123',
+    },
+  ];
+
+  constructor() {}
+
+  findUser(login: string, password: string): TUser {
+    return this.users.find(
+      (user) => user.login === login && user.password === password,
+    );
+  }
+}
