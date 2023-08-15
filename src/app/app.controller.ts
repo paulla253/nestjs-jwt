fdf648458f8e4802e14283c95042d7aee2f0cdb4
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2023-08-14 21:30:37 -0300

    feat: create controller app with use guards

---
+++ src/app/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AppService } from './app.service';
+
+@Controller()
+@UseGuards(AuthGuard('jwt'))
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
